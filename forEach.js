5253d0bcce60b3adef5e455e59ca7c3f29ba6fe0
Razib2019 <hafizurrahmanrazib@gmail.com>
2024-06-16 18:33:32 +0600

    forEach, filter and find done

---
+++ forEach.js
@@ -0,0 +1,4 @@
+// Map and forEach work as same way but forEach does not return anything where map returns an array
+const numbers = [1, 5, 6, 4, 15];
+const result = numbers.forEach((n) => console.log(n));
+console.log(result);
